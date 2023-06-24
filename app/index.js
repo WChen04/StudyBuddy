200464fc03816036ca3e3770265844154d4e1b3a
William Chen <williamchen259@gmail.com>
2023-06-24 16:44:52 -0400

    Timer

--- app/index.js
+++ app/index.js
@@ -1,47 +1,40 @@
 import { useState } from "react";
-import { SafeAreaView, ScrollView, View } from "react-native";
+import { SafeAreaView, ScrollView, View, TouchableOpacity } from "react-native";
 import { Stack, useRouter } from "expo-router";
 import { StyleSheet, Text } from "react-native";
 import Menu from "../constants/images/menu";
-import App from "./timer";
+import MyTimer from "./timer";
 
 export default function Page() {
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.main}>
         <Menu style={styles.menu}></Menu>
-        <App style={styles.app}></App>
-      </View>
-    </View>
+        <MyTimer style={styles.app} />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 4,
-    // alignItems: "center",
-    paddingLeft: 25,
-    paddingTop: 70,
+    paddingTop: 50,
+    justifyContent: "center",
   },
   main: {
-    // flex: 4,
-    // justifyContent: "flex-start",
+    flexDirection: "column",
     maxWidth: 960,
     marginHorizontal: "auto",
   },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
   menu: {
+    paddingLeft: 25,
     height: 50,
     width: 50,
     color: "black",
-    // paddingTop: 250,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 60,
+    marginLeft: 10,
   },
   app: {},
 });
--- app/timer.js
+++ app/timer.js
@@ -1,42 +1,142 @@
 import * as React from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
+import {
+  SafeAreaView,
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  useState,
+  Alert,
+  Keyboard,
+  NativeModules,
+} from "react-native";
 import Constants from "expo-constants";
 import { CountdownCircleTimer } from "react-native-countdown-circle-timer";
+import { TextInput } from "react-native-gesture-handler";
 
-export default function App() {
-  const [isPlaying, setIsPlaying] = React.useState(true);
+const children = ({ remainingTime }) => {
+  const hours = Math.floor(remainingTime / 3600);
+  const minutes = Math.floor((remainingTime % 3600) / 60);
+  const seconds = remainingTime % 60;
+  const UpdatedTime =
+    hours == 0
+      ? minutes == 0
+        ? seconds == 0
+          ? `${seconds}`
+          : `${seconds}`
+        : seconds < 10
+        ? `${minutes}:0${seconds}`
+        : `${minutes}:${seconds}`
+      : minutes < 10
+      ? seconds < 10
+        ? `${hours}:0${minutes}:0${seconds}`
+        : `${hours}:0${minutes}:${seconds}`
+      : `${hours}:${minutes}:${seconds}`;
 
+  return UpdatedTime;
+};
+
+export default function MyTimer() {
+  const [isPlaying, setIsPlaying] = React.useState(false);
+  const [number1, onChangeNumber1] = React.useState("");
+  const [number2, onChangeNumber2] = React.useState("");
+  const [number3, onChangeNumber3] = React.useState("");
+  function reset() {
+    window.location.reload(false);
+  }
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <CountdownCircleTimer
         isPlaying={isPlaying}
-        duration={60}
+        duration={number1 * 3600 + number2 * 60 + number3 * 1}
         colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
-        colorsTime={[10, 6, 3, 0]}
-        onComplete={() => ({ shouldRepeat: true, delay: 0 })}
-        updateInterval={1}
+        colorsTime={[20, 10, 5, 0]}
+        onComplete={() => Alert.alert("Done!")}
+        updateInterval={0}
+        isSmoothColorTransition={true}
       >
         {({ remainingTime, color }) => (
           <Text
-            style={{ color, fontSize: 50 }}
+            style={{ color, fontSize: 20 }}
             onPress={() => setIsPlaying((prev) => !prev)}
             suppressHighlighting={true}
           >
-            {remainingTime}
+            {children({ remainingTime })}
           </Text>
         )}
       </CountdownCircleTimer>
-    </View>
+      <Text style={{ fontSize: 25, paddingTop: 20 }}>Enter a Time:</Text>
+      <View style={styles.enterstyle}>
+        <TextInput
+          style={styles.textInputStyle}
+          onChangeText={onChangeNumber1}
+          value={number1}
+          placeholder="0"
+          placeholderTextColor={"lightgray"}
+          keyboardType="numeric"
+          returnKeyType={"done"}
+        />
+        <Text>Hours</Text>
+        <TextInput
+          style={styles.textInputStyle}
+          onChangeText={onChangeNumber2}
+          value={number2}
+          placeholder="0"
+          placeholderTextColor={"lightgray"}
+          keyboardType="numeric"
+          returnKeyType={"done"}
+        />
+        <Text>Minutes</Text>
+        <TextInput
+          style={styles.textInputStyle}
+          onChangeText={onChangeNumber3}
+          value={number3}
+          placeholder="0"
+          placeholderTextColor={"lightgray"}
+          keyboardType="numeric"
+          returnKeyType={"done"}
+        />
+        <Text>Seconds</Text>
+      </View>
+      <View style={styles.Buttons}>
+        <Button
+          color="green"
+          backgroundColor="red"
+          onPress={() => setIsPlaying((prev) => !prev)}
+          title="Start"
+        ></Button>
+        <Button onPress={reset} title="Reset"></Button>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
+    flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
     paddingTop: Constants.statusBarHeight,
     // backgroundColor: "#ecf0f1",
-    // padding: 10,
+  },
+  Buttons: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginTop: 30,
+    marginBottom: 30,
+  },
+  enterstyle: {
+    marginTop: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    // backgroundColor: "violet",
+  },
+  textInputStyle: {
+    flexDirection: "row",
+    color: "lightgrey",
+    margin: 12,
+    borderWidth: 0.5,
+    padding: 10,
   },
 });
